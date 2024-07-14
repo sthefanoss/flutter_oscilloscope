50faad30f98f9ae53b72bae36feb7ae0670d313f
sthefano <sthefanoss@gmail.com>
2024-07-14 18:33:27 -0300

    chore: change theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -5,9 +5,9 @@ const oscilloscopeActiveColor = Color(0xFF0BFC55);
 const oscilloscopeDivisionsColor = Color(0xFFBDC641);
 
 final oscilloscopeThemeData = ThemeData(
-  scaffoldBackgroundColor: const Color(0xffBAA694),
+  scaffoldBackgroundColor: const Color(0xFFBDBEC5),
   appBarTheme: const AppBarTheme(
-    backgroundColor: Color(0xFFDFE1E3),
+    backgroundColor: Color(0xFFBDBEC5),
     foregroundColor: Color(0xFF48454B),
   ),
   sliderTheme: const SliderThemeData(
